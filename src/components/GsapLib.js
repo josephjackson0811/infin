3b583a13a9a5d32c2c4d843458cff40d87748dcb
josephjackson0811 <joseph.jackson0811@gmail.com>
2024-07-15 16:18:55 -0400

    fix gsap issue

--- src/components/GsapLib.js
+++ src/components/GsapLib.js
@@ -4,6 +4,18 @@ import ScrollSmoother from 'gsap/ScrollSmoother';
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
+setTimeout(() => {
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+}, 100);
+
+setTimeout(() => {
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+}, 200);
+
+setTimeout(() => {
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+}, 300);
+
 export * from 'gsap';
 export * from 'gsap/ScrollTrigger';
 export * from 'gsap/ScrollSmoother';
